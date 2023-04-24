3e81ffd0813d756e3de7c452c2658f97e9b2eef2
Mstufing <mstufingms11@gmail.com>
2023-04-24 23:09:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+// let myArray = ["15", "11", "33", "98"];
+
+// myArray[2] = "44";
+
+// console.log(myArray);
+
+
+let myArray = ["15", "11", "33", "98"];
+myArray[5] = "44";
+
+console.log(myArray);
