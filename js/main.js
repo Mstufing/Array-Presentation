b15cc6bc794c4a4c868b460196943eafb522a7f4
Mstufing <mstufingms11@gmail.com>
2023-04-24 23:24:52 +0300

    added push and unshift methods

--- js/main.js
+++ js/main.js
@@ -7,7 +7,25 @@
 // console.log(myArray);
 
 
-let myArray = ["15", "11", "33", "98"];
-myArray[5] = "44";
+// let myArray = ["15", "11", "33", "98"];
+// myArray[5] = "44";
 
-console.log(myArray);
+// console.log(myArray);
+
+
+// ? push() 
+
+// let myFavoritePlayers = ["Messi", "Neymar", "Halland"];
+
+// myFavoritePlayers.push("Mbappe");
+
+// console.log(myFavoritePlayers);
+
+
+// ? unshift() 
+
+let myFavoritePlayers = ["Messi", "Neymar", "Halland"];
+
+myFavoritePlayers.unshift("Mbappe");
+
+console.log(myFavoritePlayers);
